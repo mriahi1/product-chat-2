49deca16c7efe26ca6c1e8f7d521e3658ee8de79
Maxime Riahi <maxriahi@gmail.com>
2023-11-19 04:59:31 +0100

    translationsDict

--- contexts/TranslationsContext.tsx
+++ contexts/TranslationsContext.tsx
@@ -1,9 +1,21 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 import translations from '@/translations';
 
-interface Translations {
+// Define your translations with an index signature
+interface LanguageDictionary {
   [key: string]: string;
 }
+
+interface Translations {
+  [language: string]: LanguageDictionary;
+}
+
+// Assuming your translations object has a similar shape to this
+const translationsDict: Translations = {
+  en: translations.en,
+  fr: translations.fr,
+  // other languages...
+};
 
 interface TranslationsContextProps {
   t: (key: string) => string;
@@ -16,8 +28,11 @@ const TranslationsContext = createContext<TranslationsContextProps | null>(null)
 export const useTranslations = () => useContext(TranslationsContext);
 
 export const TranslationsProvider = ({ children }: { children: ReactNode }) => {
-  const [language, setLanguage] = useState('en');
-  const t = (key: string) => translations[language as keyof Translations][key] || '';
+  const [language, setLanguage] = useState<string>('en');
+  const t = (key: string) => {
+    const langDictionary = translationsDict[language] || {};
+    return langDictionary[key] || '';
+  };
 
   return (
     <TranslationsContext.Provider value={{ t, language, setLanguage }}>
@@ -25,6 +40,3 @@ export const TranslationsProvider = ({ children }: { children: ReactNode }) => {
     </TranslationsContext.Provider>
   );
 };
-
-
-
