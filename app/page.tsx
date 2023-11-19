5e27b58f7783c400f85e915293c208272380a39b
Maxime Riahi <maxriahi@gmail.com>
2023-11-19 11:28:12 +0100

    center chat

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { Product } from "@/types/Product";
 import { mockProductData } from "@/data/mockProductData";
-import Modal from '@/components/Modal'; 
+import Modal from "@/components/Modal";
 import ChatBot from "@/components/ChatBot";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import ProductPage from "@/components/ProductPage";
@@ -10,9 +10,8 @@ import RecommendedProducts from "@/components/RecommendedProducts";
 import translations from "@/translations";
 // import {
 //   combinedProducts,
-// } from "./sample_data"; 
+// } from "./sample_data";
 import { TranslationsProvider } from "@/contexts/TranslationsContext";
-
 
 const Home: React.FC = () => {
   const [showOverlay, setShowOverlay] = useState<boolean>(false);
@@ -74,9 +73,10 @@ const Home: React.FC = () => {
     event.preventDefault(); // Prevent the default form submit action
     // Assuming inputMessage is the state used for handling input value
     const searchTerm = inputMessage.trim().toLowerCase();
-    const searchResults = products.filter((product) =>
-      product.title.toLowerCase().includes(searchTerm) ||
-      product.description.toLowerCase().includes(searchTerm)
+    const searchResults = products.filter(
+      (product) =>
+        product.title.toLowerCase().includes(searchTerm) ||
+        product.description.toLowerCase().includes(searchTerm)
     );
     // Handle the search logic, for example, updating state with the results
     // or calling a function to display the results
@@ -88,31 +88,36 @@ const Home: React.FC = () => {
     <TranslationsProvider>
       {/* Overlay for initial search */}
       {showOverlay && (
-      <div className="fixed inset-0 bg-gray-900 bg-opacity-95 flex justify-center items-center p-4">
-        <div className="w-full max-w-lg p-5 bg-gray-700 rounded-lg">
-          <h2 className="text-xl text-white mb-4">{t?.('searching_for_products')}</h2>
-          <form onSubmit={handleSearchSubmit} className="space-y-4">
-            <label htmlFor="searchInput" className="block text-sm font-medium text-gray-200">
-              {t?.('search_explainer')}
-            </label>
-            <input
-              id="searchInput"
-              type="text"
-              placeholder={t?.('search_placeholder')}
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              value={inputMessage}
-              onChange={(e) => setInputMessage(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            >
-              {t?.('search')}
-            </button>
-          </form>
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-95 flex justify-center items-center p-4">
+          <div className="w-full max-w-lg p-5 bg-gray-700 rounded-lg">
+            <h2 className="text-xl text-white mb-4">
+              {t?.("searching_for_products")}
+            </h2>
+            <form onSubmit={handleSearchSubmit} className="space-y-4">
+              <label
+                htmlFor="searchInput"
+                className="block text-sm font-medium text-gray-200"
+              >
+                {t?.("search_explainer")}
+              </label>
+              <input
+                id="searchInput"
+                type="text"
+                placeholder={t?.("search_placeholder")}
+                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              >
+                {t?.("search")}
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    )}
+      )}
 
       {/* Signup Modal */}
       {showSignupModal && (
@@ -120,67 +125,77 @@ const Home: React.FC = () => {
           {/* Replace with your signup form */}
           <form>
             {/* Form fields */}
-            <button type="submit">{t('signup')}</button>
+            <button type="submit">{t("signup")}</button>
           </form>
         </Modal>
       )}
 
-    <div className="container">
-      <header className="flex justify-between items-center p-4 header">
-        <div className="logo">
-          <h1>{t("logo")}</h1>
-        </div>
-       <div className="profile" onClick={handleProfileClick}>
-          <h1>{t("search_history")}</h1>
-        </div>
-      </header>
+      <div className="container">
+        <header className="flex justify-between items-center p-4 header">
+          <div className="logo">
+            <h1>{t("logo")}</h1>
+          </div>
+          <div className="profile" onClick={handleProfileClick}>
+            <h1>{t("search_history")}</h1>
+          </div>
+        </header>
 
-      <main className="flex flex-col md:flex-row">
-        <aside className="w-full md:w-3/12 p-4 h-full">
-          <ChatBot
-            productData={products}
-            onProductSelect={updateSelectedProduct}
-          />
-          <button onClick={() => setLanguage(language === "en" ? "fr" : "en")}>
-            {t("switch_language")}
-          </button>
-        </aside>
-
-        <section className="w-full md:w-5/12 p-4">
+        <main
+          className={`flex flex-col md:flex-row ${
+            selectedProduct ? "" : "justify-center"
+          }`}
+        >
           {selectedProduct ? (
-          <FeaturedProduct {...selectedProduct} />
-          ): (
-          <></>
+            <>
+              <aside className="w-full md:w-3/12 p-4 h-full">
+                <ChatBot
+                  productData={products}
+                  onProductSelect={updateSelectedProduct}
+                />
+                <button
+                  onClick={() => setLanguage(language === "en" ? "fr" : "en")}
+                >
+                  {t("switch_language")}
+                </button>
+              </aside>
+
+              <section className="w-full md:w-5/12 p-4">
+                <FeaturedProduct {...selectedProduct} />
+              </section>
+
+              <aside className="w-full md:w-4/12 p-4">
+                <RecommendedProducts
+                  similarProducts={similarProducts}
+                  alternativeProducts={alternativeProducts}
+                  onProductSelect={updateSelectedProduct}
+                />
+              </aside>
+            </>
+          ) : (
+            <>
+              <aside className="w-full md:w-6/12 p-4">
+                <ChatBot
+                  productData={products}
+                  onProductSelect={updateSelectedProduct}
+                />
+              </aside>
+            </>
           )}
-        </section>
+        </main>
 
-        <aside className="w-full md:w-4/12 p-4">
-        {selectedProduct ? (
-          <RecommendedProducts
-            similarProducts={similarProducts}
-            alternativeProducts={alternativeProducts}
-            onProductSelect={updateSelectedProduct}
-          />
-          ): (
-            <></>
-            )}
-        </aside>
-      </main>
-
-      <main className="flex flex-col md:flex-row">
-        {/* <aside className="w-full md:w-6/12 p-4">
+        <main className="flex flex-col md:flex-row">
+          {/* <aside className="w-full md:w-6/12 p-4">
           <ProductsList />
         </aside> */}
-        {/* <aside className="w-full md:w-6/12 p-4">
+          {/* <aside className="w-full md:w-6/12 p-4">
           <ProductPage />
         </aside> */}
-      </main>
+        </main>
 
-      <footer className="p-4">{/* Footer content here */}</footer>
-    </div>
+        <footer className="p-4">{/* Footer content here */}</footer>
+      </div>
     </TranslationsProvider>
   );
-  
 };
 
 export default Home;
