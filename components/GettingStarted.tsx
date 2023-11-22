486050730d71cd75324b9fda5532d78f8dcfb31b
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-11-22 23:05:05 +0100

    Update GettingStarted.tsx

--- components/GettingStarted.tsx
+++ components/GettingStarted.tsx
@@ -7,7 +7,7 @@ const GettingStarted: React.FC = () => {
   return (
     <div className="flex flex-col bg-white border-r border-gray-200 w-80 p-4">
       <h1 className="text-xl font-semibold mb-2">{t('eco_chatbot_title')}</h1>
-      <p className="text-gray-600 mb-4">{t('eco_chatbot_purpose')}</p>
+       <p className="text-gray-600 mb-4" style={{ whiteSpace: 'pre-line' }}>{t('eco_chatbot_purpose')}</p>
      
     </div>
   );
