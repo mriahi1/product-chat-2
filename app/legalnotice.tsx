6459f6b53c04e743930bcf243a3e9d4276d63e3e
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-11-24 12:22:40 +0100

    Create legalnotice.tsx

---
+++ app/legalnotice.tsx
@@ -0,0 +1,13 @@
+import React from 'react',
+import { useTranslation } from "@/contexts/TranslationsContext",
+
+const MentionsLegales: React.FC = () => {
+  return (
+    <div>
+      <h1>Mentions Légales</h1>
+      {/* Ajoutez ici le contenu de vos mentions légales */}
+    </div>
+  );
+}
+
+export default MentionsLegales;
