9f70a6be1918d108b42c5242d98982976c0f0b70
Max Riahi <mriahi1@users.noreply.github.com>
2023-11-28 21:24:45 +0100

    Update legalnotice.tsx

--- app/legalnotice.tsx
+++ app/legalnotice.tsx
@@ -7,7 +7,7 @@ const MentionsLegales: React.FC = () => {
       <h1>Mentions Légales</h1>
       {/* Ajoutez ici le contenu de vos mentions légales */}
     </div>
-  );
+  )
 }
 
-export default MentionsLegales
+export default MentionsLegales;
