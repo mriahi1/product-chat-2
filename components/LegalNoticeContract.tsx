12ff07bcd35edbfed9d631b67e5134c7058cd4d5
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-11-29 11:25:17 +0100

    Update LegalNoticeContract.tsx

--- components/LegalNoticeContract.tsx
+++ components/LegalNoticeContract.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { useTranslation } from '@/contexts/TranslationsContext';
 
 const MentionsLegales: React.FC = () => {
   const { t } = useTranslation();
-  
+
+  const legalMentionsText = t?.('legal_mention_contract');
+
+  // Remplacer les ** par des balises <strong>
+  const formattedText = legalMentionsText?.split('**').map((segment, index) => (
+    index % 2 === 0 ? segment : <strong key={index}>{segment}</strong>
+  ));
+
+  // Remplacer les \n par des balises <br>
+  const textWithLineBreaks = formattedText?.join('\n');
+
   return (
     <div>
       <h1>{t?.('legal_mention')}</h1>
-      <p>
-      {t?.('legal_mention_contract')}
-      </p>
+      <p style={{ whiteSpace: 'pre-line' }}>{textWithLineBreaks}</p>
     </div>
-  )
-}
+  );
+};
 
 export default MentionsLegales;
