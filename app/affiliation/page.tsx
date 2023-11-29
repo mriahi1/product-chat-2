3feb993ae2ead86defac7c0be0482265163ba9a0
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-11-29 11:11:23 +0100

    Update page.tsx

--- app/affiliation/page.tsx
+++ app/affiliation/page.tsx
@@ -1,1 +1,32 @@
-AA
+"use client";
+import React from 'react';
+import type { NextPage } from 'next';
+import Header from '@/components/Header';
+import AffiliationStatement from '@/components/AffiliationStatement';
+import { TranslationProvider } from '@/contexts/TranslationsContext';
+import AffiliationStatementContract from '@/components/AffiliationStatementContract';
+
+const Affiliation: NextPage = () => {
+
+  return (
+    <TranslationProvider>
+      
+
+    <div className="container">
+      <Header />
+      
+      <main
+          className={`flex flex-col md:flex-row`}
+        >
+         <AffiliationStatementContract />
+        </main>
+
+        <footer>
+          <AffiliationStatement />
+        </footer>
+      </div>
+    </TranslationProvider>
+  );
+};
+
+export default Affiliation;
