11e3ec4ffbd781def4214b12c2c15f8c923968d8
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-11-29 11:07:38 +0100

    Create AffiliationStatement.tsx

---
+++ components/AffiliationStatement.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useTranslation } from '@/contexts/TranslationsContext';
+import Link from 'next/link';
+
+const AffiliationStatement: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <Link href="/affiliation">{t?.('affiliation_mention')}</Link>
+    </div>
+  )
+}
+
+export default AffiliationStatement;
