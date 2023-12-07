027c0b60633a548aa9babf09a187749a213c9197
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-12-07 23:57:25 +0100

    Fixed margin top

--- app/legal/page.tsx
+++ app/legal/page.tsx
@@ -2,25 +2,17 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Header from '@/components/Header';
-import LegalNotice from '@/components/LegalNotice';
-import { TranslationProvider } from '@/contexts/TranslationsContext';
 import LegalNoticeContract from '@/components/LegalNoticeContract';
+import { TranslationProvider } from '@/contexts/TranslationsContext';
 
 const Legal: NextPage = () => {
-
   return (
     <TranslationProvider>
-      
-
-    <div className="container">
-      <Header />
-      
-      <main
-          className={`flex flex-col md:flex-row`}
-        >
-         <LegalNoticeContract />
+      <div className="container" style={{ marginTop: '100px' }}>
+        <Header />
+        <main className={`flex flex-col md:flex-row`}>
+          <LegalNoticeContract />
         </main>
-
       </div>
     </TranslationProvider>
   );
