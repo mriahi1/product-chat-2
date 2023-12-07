9ae50a60b8197dc86f76044a523e77399d0ea82f
bastienj1 <151460176+bastienj1@users.noreply.github.com>
2023-12-07 23:57:04 +0100

    Fixed margin top

--- app/affiliation/page.tsx
+++ app/affiliation/page.tsx
@@ -2,25 +2,17 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Header from '@/components/Header';
-import AffiliationStatement from '@/components/AffiliationStatement';
-import { TranslationProvider } from '@/contexts/TranslationsContext';
 import AffiliationStatementContract from '@/components/AffiliationStatementContract';
+import { TranslationProvider } from '@/contexts/TranslationsContext';
 
 const Affiliation: NextPage = () => {
-
   return (
     <TranslationProvider>
-      
-
-    <div className="container">
-      <Header />
-      
-      <main
-          className={`flex flex-col md:flex-row`}
-        >
-         <AffiliationStatementContract />
+      <div className="container" style={{ marginTop: '100px' }}>
+        <Header />
+        <main className={`flex flex-col md:flex-row`}>
+          <AffiliationStatementContract />
         </main>
-
       </div>
     </TranslationProvider>
   );
