626018db82a645a0a06f1ed0748ee4b2003df0be
Max Riahi <mriahi1@users.noreply.github.com>
2024-03-24 19:26:59 +0100

    blog

--- components/blog/BlogPost.tsx
+++ components/blog/BlogPost.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import BlogPost from '@/types/BlogPost';
 import { useSearchParams } from 'next/navigation'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface BlogPostProps {
 }
